8fb3c6881e04861155dec3a8a5d99d2f66d19716
antytyp <pwdyrlaga@gmail.com>
2024-09-27 20:52:36 +0200

    add test_str_length

--- tests/test_pandera/test_pandera_checks.py
+++ tests/test_pandera/test_pandera_checks.py
@@ -8,6 +8,7 @@ def sample_dataframe():
     return pd.DataFrame({
         'numerical_column': [1.1, 2.5, 3.6, 4.8, 1.1, -2.5, 3.6, 0],
         'constant_column': [0, 0, 0, 0, 0, 0, 0, 0],
+        'string_column': ['aa', 'abbb', 'acc', 'a', 'bc', 'abcd', 'c', 'ab'],
     })
 
 
@@ -37,7 +38,7 @@ class TestPanderaChecks:
         with pytest.raises(pa.errors.SchemaError):
             invalid_schema.validate(sample_dataframe)
 
-    def test_equal_to(self, sample_dataframe):
+    def test_check_equal_to(self, sample_dataframe):
         valid_schema = pa.DataFrameSchema({
             "constant_column": pa.Column(int, pa.Check.equal_to(value=0))
         })
@@ -49,7 +50,7 @@ class TestPanderaChecks:
         with pytest.raises(pa.errors.SchemaError):
             invalid_schema.validate(sample_dataframe)
 
-    def test_isin(self, sample_dataframe):
+    def test_check_isin(self, sample_dataframe):
         valid_schema = pa.DataFrameSchema({
             "constant_column": pa.Column(int, pa.Check.isin(allowed_values=[0, 1, 2]))
         })
@@ -57,6 +58,18 @@ class TestPanderaChecks:
 
         invalid_schema = pa.DataFrameSchema({
             "numerical_column": pa.Column(int, pa.Check.isin(allowed_values=[1.1, 2.5, 3.6]))
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            invalid_schema.validate(sample_dataframe)
+
+    def test_str_length(self, sample_dataframe):
+        valid_schema = pa.DataFrameSchema({
+            "string_column": pa.Column(str, pa.Check.str_length(min_value=1, max_value=5))
+        })
+        valid_schema.validate(sample_dataframe)
+
+        invalid_schema = pa.DataFrameSchema({
+            "string_column": pa.Column(str, pa.Check.str_length(min_value=4, max_value=5))
         })
         with pytest.raises(pa.errors.SchemaError):
             invalid_schema.validate(sample_dataframe)
