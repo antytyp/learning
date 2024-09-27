a044c5d94fd1b8f0049182db971312fc5c2051f8
antytyp <pwdyrlaga@gmail.com>
2024-09-27 20:34:38 +0200

    adjust test_check_greater_than

--- tests/test_pandera/test_pandera_checks.py
+++ tests/test_pandera/test_pandera_checks.py
@@ -3,25 +3,26 @@ import pandera as pa
 import pytest
 
 
+@pytest.fixture(scope="class")
+def sample_dataframe():
+    return pd.DataFrame({
+        'numerical_column': [1.1, 2.5, 3.6, 4.8, 1.1, -2.5, 3.6, 0],
+    })
+
+
 class TestPanderaChecks:
 
-    def test_check_greater_than(self):
-        schema = pa.DataFrameSchema({
-            "positive_column": pa.Column(float, pa.Check.greater_than(0))
+    def test_check_greater_than(self, sample_dataframe):
+        valid_schema = pa.DataFrameSchema({
+            "numerical_column": pa.Column(float, pa.Check.greater_than(-3.0))
         })
+        valid_schema.validate(sample_dataframe)
 
-        valid_df = pd.DataFrame({
-            "positive_column": [1.1, 2.5, 3.6, 4.8]
+        invalid_schema = pa.DataFrameSchema({
+            "numerical_column": pa.Column(float, pa.Check.greater_than(0.0))
         })
-
-        invalid_df = pd.DataFrame({
-            "positive_column": [1.1, -2.5, 3.6, 0]
-        })
-
-        schema.validate(valid_df)
-
         with pytest.raises(pa.errors.SchemaError):
-            schema.validate(invalid_df)
+            invalid_schema.validate(sample_dataframe)
 
     def test_check_in_range(self):
         schema = pa.DataFrameSchema({
