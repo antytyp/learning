98a473cedcc7de2a182978ee2281daf9278b065d
antytyp <pwdyrlaga@gmail.com>
2024-10-14 20:00:06 +0200

    update pa.DataFrameSchema description

--- src/pandera_playground/titanic_data_frame_schema.py
+++ src/pandera_playground/titanic_data_frame_schema.py
@@ -1,23 +1,31 @@
 import pandera as pa
 
 # pa.DataFrameSchema arguments explained:
-# columns: Optional[Dict[Any, Any]] = None, - defines the schema for individual columns using pa.Column
-# checks: Optional[CheckList] = None, - allows validation rules for the entire DataFrame
-# parsers: Optional[ParserList] = None, - TODO
-# index = None, - defines validation rules for the DataFrame’s index
-# dtype: Optional[Any] = None, - TODO
-# coerce: bool = False, - automatically converts columns to the specified data types
-# strict: StrictType = False, - disallows columns not defined in the schema
-# name: Optional[str] = None, - gives the schema a custom name for easier logging and debugging
-# ordered: bool = False, - ensures that the DataFrame columns match the schema’s column order
-# unique: Optional[Union[str, List[str]]] = None, - TODO
-# report_duplicates: UniqueSettings = "all", - TODO
-# unique_column_names: bool = False, - TODO
-# add_missing_columns: bool = False, - TODO
-# title: Optional[str] = None, - TODO
-# description: Optional[str] = None, - TODO
-# metadata: Optional[dict] = None, - TODO
-# drop_invalid_rows: bool = False, - TODO
+# -> columns – a dict where keys are column names and values are Column objects, specifying the datatypes and properties
+# of a particular column.
+# -> checks - dataframe-wide checks.
+# -> parsers - dataframe-wide parsers.
+# -> index - specify the datatypes and properties of the index.
+# -> dtype -  datatype of the dataframe. This overrides the data types specified in any of the columns. If a string is
+# specified, then assumes one of the valid pandas string values:
+# http://pandas.pydata.org/pandas-docs/stable/basics.html#dtypes.
+# -> coerce - whether or not to coerce all of the columns on validation. This overrides any coerce setting at the
+# column or index level. This has no effect on columns where dtype=None.
+# -> strict - ensure that all and only the columns defined in the schema are present in the dataframe. If set to
+# ‘filter’, only the columns in the schema will be passed to the validated dataframe. If set to filter and columns
+# defined in the schema are not present in the dataframe, will throw an error.
+# -> name - name of the schema.
+# -> ordered - whether or not to validate the columns order.
+# -> unique - a list of columns that should be jointly unique.
+# -> report_duplicates -  how to report unique errors - exclude_first: report all duplicates except first occurence
+# - exclude_last: report all duplicates except last occurence - all: (default) report all duplicates
+# -> unique_column_names - whether or not column names must be unique.
+# -> add_missing_columns - add missing column names with either default value, if specified in column schema, or NaN if
+# column is nullable.
+# -> title - a human-readable label for the schema.
+# -> description - an arbitrary textual description of the schema.
+# -> metadata - an optional key-value data.
+# -> drop_invalid_rows - if True, drop invalid rows on validation.
 
 # pa.Column arguments explained:
 # dtype: PandasDtypeInputTypes = None, - specifies the data type
