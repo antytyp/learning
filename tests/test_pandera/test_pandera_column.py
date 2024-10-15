13e9541d884966ee6fa7c881fa92c6e8f31f6e2b
antytyp <pwdyrlaga@gmail.com>
2024-10-15 20:22:48 +0200

    wip implement more tests

--- tests/test_pandera/test_pandera_column.py
+++ tests/test_pandera/test_pandera_column.py
@@ -59,37 +59,190 @@ class TestPanderaColumn:
             _ = schema.validate(invalid_df)
 
     def test_column_nullable(self):
-        pass
+        column_validator = pa.Column(
+            nullable=False,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        valid_df = pd.DataFrame({
+            "A": [1, 2, 3],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "A": [1, None, 3],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
 
     def test_column_unique(self):
-        pass
+        column_validator = pa.Column(
+            unique=True,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
 
-    def test_column_report_nullables(self):
+        valid_df = pd.DataFrame({
+            "A": [1, 2, 3],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "A": [1, 2, 2],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
+
+    def test_column_report_duplicates(self):
+        # (Union[Literal[‘exclude_first’], Literal[‘exclude_last’], Literal[‘all’]]) – how to report unique errors -
+        # exclude_first: report all duplicates except first occurence - exclude_last: report all duplicates except
+        # last occurence - all: (default) report all duplicates
+        # TODO
         pass
 
     def test_column_coerce(self):
-        pass
+        column_validator = pa.Column(
+            dtype=int,
+            coerce=True,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        df = pd.DataFrame({
+            "A": ["1", "2", "3"],
+        })
+        assert df["A"].dtype != 'int64'
+        validated_df = schema.validate(df)
+        assert validated_df["A"].dtype == 'int64'
 
     def test_column_required(self):
-        pass
+        column_validator = pa.Column(
+            required=True,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        valid_df = pd.DataFrame({
+            "A": [1, 2, 3],
+            "B": [4, 5, 6],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "B": [4, 5, 6],
+            "C": [1, 2, 3],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
 
     def test_column_name(self):
-        pass
+        column_validator = pa.Column(
+            name=None,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        valid_df = pd.DataFrame({
+            "A": [],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "A": [],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
 
     def test_column_regex(self):
-        pass
+        column_validator = pa.Column(
+            regex=None,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        valid_df = pd.DataFrame({
+            "A": [],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "A": [],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
 
     def test_column_title(self):
-        pass
+        column_validator = pa.Column(
+            title=None,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        valid_df = pd.DataFrame({
+            "A": [],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "A": [],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
 
     def test_column_description(self):
-        pass
+        column_validator = pa.Column(
+            description=None,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        valid_df = pd.DataFrame({
+            "A": [],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "A": [],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
 
     def test_column_default(self):
-        pass
+        column_validator = pa.Column(
+            default=0.0,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        df = pd.DataFrame({
+            "A": [1.0, None, 3.0],
+        })
+        validated_df = schema.validate(df)
+        assert validated_df["A"].notna().all()
 
     def test_column_metadata(self):
-        pass
+        column_validator = pa.Column(
+            metadata=None,
+        )
+        schema = pa.DataFrameSchema({"A": column_validator})
+
+        valid_df = pd.DataFrame({
+            "A": [],
+        })
+        _ = schema.validate(valid_df)
+
+        invalid_df = pd.DataFrame({
+            "A": [],
+        })
+        with pytest.raises(pa.errors.SchemaError):
+            _ = schema.validate(invalid_df)
 
     def test_column_drop_invalid_rows(self):
-        pass
+        column_validator = pa.Column(
+            name="A",
+            drop_invalid_rows=True,
+            checks=pa.Check(check_fn=lambda x: x % 5 == 0),
+        )
+        df = pd.DataFrame({
+            "A": [5, 10, 12, 15]
+        })
+        validated_df = column_validator.validate(df, lazy=True)
+        expected_validated_df = pd.DataFrame({
+            "A": [5, 10, 15]
+        })
+        pd.testing.assert_frame_equal(
+            validated_df.reset_index(drop=True),
+            expected_validated_df.reset_index(drop=True)
+        )
